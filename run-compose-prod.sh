bd18182084420e6df7ad40a6862ed48fce1c6ebf
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2024-02-17 13:04:54 -0500

    added environment variables to run-compose-prod and docker-compose-prod

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -9,6 +9,13 @@ export SECRET_KEY=$1
 export DEBUG=$2
 export NEW_VERSION=$3
 export DOCKERHUB_UNAME=$4
+export EMAIL=$5
+export GMAIL_APP_PASSWORD=$6
+export DB_PORT=$7
+export HOST=$8
+export POSTGRES_DB=$9
+export POSTGRES_USER=$10
+export POSTGRES_PASSWORD=$11
 
 
 docker-compose -f docker-compose.prod.yml build --no-cache
