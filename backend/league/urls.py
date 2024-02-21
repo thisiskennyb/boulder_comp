e10c875d17cd4afc542b92f7bf8605f760e48967
successPhil <129565748+successPhil@users.noreply.github.com>
2024-02-21 01:59:20 -0500

    added view to reive all leagues a user is in, get specific league, and also all leagues. Added similar views for teams. All teams a user is on, all teams in a league, and a specifc team

--- backend/league/urls.py
+++ backend/league/urls.py
@@ -5,6 +5,8 @@ from league.views import LeagueView, CreateLeagueTeamView
 urlpatterns = [
     
     path('', LeagueView.as_view(), name='league'),
+    path('<int:pk>', LeagueView.as_view(), name='league'),
+    path('all/', LeagueView.as_view(), name='league'),
     path('create_team/', CreateLeagueTeamView.as_view(), name='create-team'),
     path('create_team/<int:pk>', CreateLeagueTeamView.as_view(), name='view-team')
 ]
--- backend/team/views.py
+++ backend/team/views.py
@@ -6,9 +6,36 @@ from .models import Team
 from league.serializers import TeamSerializer
 
 
+class TeamsInLeagueView(APIView):
+    #Gets all teams for a specific league, requires league_id
+    def get(self, request):
+        data = request.data
 
+        league_teams = Team.objects.filter(league=data['league_id'])
+        serializer = TeamSerializer(league_teams, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class AllTeamsView(APIView):
+    #Gets all teams
+    def get(self, request):
+        all_teams = Team.objects.all()
+        serializer = TeamSerializer(all_teams, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 class TeamView(APIView):
+    #Get specific team with team_id
+    def get(self, request):
+        #Get a specific team, requires id
+        data = request.data
+        team = Team.objects.get(id=data['team_id'])
+        serializer = TeamSerializer(team)
+
+        return Response(serializer.data)
+
+
+##### Needs some validation or logic in add_team_member method
+## Probably can query League to check team_size and only allow add_team_member to function when members <= team_size
     def post(self, request):
+        #Join a team in a a league, requires league_id, team_id
         user = request.user
         data = request.data
 
