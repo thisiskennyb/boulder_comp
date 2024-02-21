f8fce7aa14c4d9bdf0ea860ff3c9bfa4b6999da4
successPhil <129565748+successPhil@users.noreply.github.com>
2024-02-21 14:29:07 -0500

    built async function to fetch leagues that a user belongs to. Implemented useEffect in user dashboard route to grab this data and set it in state when available

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,4 +1,30 @@
+import { leaguesUserIsIn } from "../api/backend_calls"
+import { useEffect, useState } from "react"
+
 export default function Dashboard() {
+    // Declaring state for what we need. Currently: dashboard leagues --> all of the leagues a user is in
+    const [ dashboardLeagues, setDashboardLeagues ] = useState({})
+
+    // Async function to use our async fetch from backend_calls
+    const userLeagues = async () =>{
+        const leagues = await leaguesUserIsIn()
+        if (leagues){
+            setDashboardLeagues(leagues)
+        }
+    }
+
+    // Use effect to run once.
+    useEffect(()=>{
+        userLeagues() // Will update state if it changes
+    }, [])
+
+
+    // For now just console logging state is being grabbed when we navigate to dashboard.
+    // Currently getting all the leagues a user is in
+    console.log(dashboardLeagues)
+
+    
+
     return (
     <>
     <h1>Welcome to your dashboard!</h1>
