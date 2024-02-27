d701e65abb5030638933e6300c4d712163e61283
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2024-02-26 20:44:02 -0500

    front end changes

--- frontend/src/components/DashboardSends.jsx
+++ frontend/src/components/DashboardSends.jsx
@@ -1,6 +1,16 @@
-export default function DashboardSends(){
+export default function DashboardSends({userSends}){
     return (
 
-        <>This retrieves all of the sends for a user</>
+        <>
+                            {userSends.length > 0 && (
+                        userSends.map((send) => (
+                            <div key={send.id}>
+                                <h2>Boulder: {send.boulder.name}</h2>
+                                <p>grade: {send.boulder.grade}</p>
+                                <p>date: {send.send_date}</p>
+                            </div>
+                        ))
+                    )}
+        </>
     )
 }
