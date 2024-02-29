86a7a242a2ad2e5b2ef58bc4ef91f774a90973e6
successPhil <129565748+successPhil@users.noreply.github.com>
2024-02-29 05:34:53 -0500

    Updated League Teams page to keep styling consistent. Removed old logic in TeamSendTable, tested mobile and web display

--- frontend/src/components/TeamSendTable.jsx
+++ frontend/src/components/TeamSendTable.jsx
@@ -2,46 +2,38 @@ import React from "react";
 
 const TeamSendTable = ({ teamSends }) => {
   return (
-    <div className="overflow-x-auto">
-      {/* Desktop/Tablet View */}
-      <table className="hidden md:table table-auto w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 w-1/5">Boulder Name</th>
-            <th className="px-4 py-2 w-1/5">Grade</th>
-            <th className="px-4 py-2 w-1/5">Username</th>
-            <th className="px-4 py-2 w-1/5">Send Date</th>
-            <th className="px-4 py-2 w-1/5">Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teamSends.map((send, index) => (
-            <tr key={index}>
-              <td className="text-center px-4 py-2 w-1/5">{send.boulder}</td>
-              <td className="text-center px-4 py-2 w-1/5">{send.grade}</td>
-              <td className="text-center px-4 py-2 w-1/5">{send.username}</td>
-              <td className="text-center px-4 py-2 w-1/5">{send.send_date}</td>
-              <td className="text-center px-4 py-2 w-1/5">{send.score}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
 
-      {/* Mobile View */}
-      <div className="flex flex-col md:hidden items-center divide-y divide-blue-700">
-        {teamSends.map((send, index) => (
-          <div key={index}>
-            
-            <p><strong>Boulder:</strong> {send.boulder}</p>
-            <p><strong>Grade:</strong> {send.grade}</p>
-            <p><strong>Username:</strong> {send.username}</p>
-            <p><strong>Send Date:</strong> {send.send_date}</p>
-            <p><strong>Score:</strong> {send.score}</p>
-            {index !== teamSends.length - 1 && <hr className="border-2 divide-blue-700 mb-4" />}
-          </div>
-        ))}
+    <div className="">
+      <div className="flex bg-gray-800 w-11/12 md:w-4/5 mx-auto rounded-lg mt-8 items-center">
+        <h2 className="text-white text-base md:text-4xl font-nunito w-1/5 text-center">Boulder</h2>
+        <h2 className="text-white text-base font-nunito md:text-3xl w-1/5 text-center">Grade</h2>
+        <h2 className="text-white text-base font-nunito md:text-3xl w-1/3 text-center">Username</h2>
+        <h2 className="text-white text-base font-nunito md:text-3xl w-1/5 text-center">Sent</h2>
+        <h2 className="text-white text-base font-nunito md:text-3xl w-1/5 text-center">Score</h2>
+        
       </div>
+      <div className="flex">
+        <hr className="w-11/12 md:w-4/5"></hr>
+      </div>
+
+      {teamSends?.length > 0 && (
+        teamSends.map((send, index) => (
+            <div key={send.id} className="flex flex-col">
+                <div className="flex bg-gray-700 w-11/12 md:w-4/5 mx-auto rounded-lg items-center">
+                <h2 className="text-white text-sm md:text-xl font-nunito w-1/5 text-center">{send.boulder}</h2>
+                <p className="text-white text-sm md:text-xl font-nunito w-1/5 text-center">{send.grade}</p>
+                <p className="text-white text-sm md:text-xl font-nunito w-1/3 text-center">{send.username}</p>
+                <p className="text-white text-sm md:text-xl font-nunito w-1/5 text-center">{send.send_date}</p>
+                <p className="text-white text-sm md:text-xl font-nunito w-1/5 text-center">{send.score}</p>
+                
+
+                </div>
+                <hr className="w-11/12 md:w-4/5"></hr>
+            </div>            
+                ))
+            )}
     </div>
+
   );
 };
 
