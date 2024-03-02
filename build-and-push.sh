7039b73cb4efa5d7a2adeabe2a961b4a81e86923
thisiskennyb <kendbonnette@gmail.com>
2024-03-02 09:20:53 -0500

    changed build-and-push build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -15,23 +15,15 @@
 BASE_URL=$1
 NEW_VERSION=$2
 DOCKERHUB_UNAME=$3
-POSTGRES_DB=$4
-POSTGRES_USER=$5
-POSTGRES_PASSWORD=$6
-EMAIL=$7
-GMAIL_APP_PASSWORD=$8
-DB_PORT=$9
-HOST=${10}
-AWS_ACCESS_KEY_ID=${11}
-AWS_SECRET_ACCESS_KEY=${12}
 
 
 
-docker build --build-arg VITE_BASE_URL=$BASE_URL -t $DOCKERHUB_UNAME/boulder_comp_webserver-prod:$NEW_VERSION -f webserver/Dockerfile . --no-cache
+
+# docker build --build-arg VITE_BASE_URL=$BASE_URL -t $DOCKERHUB_UNAME/boulder_comp_webserver-prod:$NEW_VERSION -f webserver/Dockerfile . --no-cache
+# docker push $DOCKERHUB_UNAME/boulder_comp_webserver-prod:$NEW_VERSION
+
+docker build -t VITE_BASE_URL=$BASE_URL $DOCKERHUB_UNAME/boulder_comp_webserver-prod:$NEW_VERSION -f webserver/Dockerfile . --no-cache
 docker push $DOCKERHUB_UNAME/boulder_comp_webserver-prod:$NEW_VERSION
-
-# docker build -t VITE_BASE_URL=$BASE_URL $DOCKERHUB_UNAME/book_club_webserver-prod:$NEW_VERSION -f webserver/Dockerfile . --no-cache
-# docker push $DOCKERHUB_UNAME/book_club_webserver-prod:$NEW_VERSION
 
 docker build -t $DOCKERHUB_UNAME/boulder_comp_api-prod:$NEW_VERSION -f backend/Dockerfile ./backend --no-cache
 docker push $DOCKERHUB_UNAME/boulder_comp_api-prod:$NEW_VERSION
