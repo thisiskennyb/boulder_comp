692c061b4535b2139d8607a8693bd7a1f409269b
successPhil <129565748+successPhil@users.noreply.github.com>
2024-03-07 13:34:59 -0500

    fixed signup message to be responsive to mobile and web displays, removed unneccessary state for Response Message. Toast notification message is sufficient to alert user

--- frontend/src/routes/SignupMessage.jsx
+++ frontend/src/routes/SignupMessage.jsx
@@ -1,10 +1,12 @@
 export default function SignupMessage() {
     return (
-      <div className="flex items-center justify-center mt-20 mx-4">
-        <div className="flex flex-col shadow-lg border-solid border w-20px items-center max-w-md p-4 mb-4 bg-gray-100 rounded-md">
-          <h1 className="font-rubik text-center">Thank you for signing up!</h1>
-          <div className="font-nunito text-center">To activate your account, please check your email and click on the activation link we've sent you. If you don't see the email within a few minutes, please check your spam or junk folder.</div>
+      <div className="bg-night min-h-screen">
+      <div className="flex items-center justify-center py-20 bg-night">
+        <div className="flex flex-col shadow-lg border-solid border-2 w-1/2 md:w-1/3 items-center p-4 bg-gray-800 rounded-md border-white">
+          <h1 className="font-nunito text-center text-white text-3xl md:text-4xl">Thank you for signing up!</h1>
+          <div className="font-nunito text-center text-white text-base md:text-xl">To activate your account, please check your email and click on the activation link we've sent you. If you don't see the email within a few minutes, please check your spam or junk folder.</div>
         </div>
+      </div>
       </div>
     );
   }
