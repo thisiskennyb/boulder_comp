a922a529b4b33e554f743406d1be2d10a496b430
successPhil <129565748+successPhil@users.noreply.github.com>
2024-03-09 05:10:58 -0500

    added framer motion and created GradeBar component to display dashboard send feed data. Userdashboard now displays users personal sends. Need to implement filter to ensure only recent sends are displayed for feed and grade bars

--- frontend/src/components/DashboardSends.jsx
+++ frontend/src/components/DashboardSends.jsx
@@ -1,9 +1,35 @@
-export default function DashboardSends({userSends, handleLogSend}){
+import GradeBar from "./GradeBar";
 
+export default function DashboardSends({userSends, handleLogSend}){
+    // This function updates the frequency for user sends
+    // Creates a object with {grade: frequency}
+
+    const gradeCounts = userSends.reduce((acc, send) => {
+        const grade = send.boulder.grade;
+        // accumulate the grade if it exist + 1 or 0 if it does not exist
+        acc[grade] = (acc[grade] || 0) + 1;
+        return acc;
+    }, {})
+
+    // Can also get a total number of sends by reducing the values of gradeCounts
+    const totalSends = Object.values(gradeCounts).reduce((acc, count) => acc + count, 0);
+
+    // Sorting gradeCounts by grade before mapping
+    //Map bars in order to give proper pyramid
+    const gradeBars = Object.entries(gradeCounts)
+    .sort(([gradeA], [gradeB]) => gradeA.localeCompare(gradeB)) // Sort the grade keys
+    .map(([grade, count]) => (
+        <GradeBar key={grade} grade={grade} count={count} total={totalSends} barColor={"#F4AC45"} />
+    ));
     
     return (
-        <div>
+        <div className="h-screen">
+            <div className="flex justify-center text-white font-nunito-black font-extrabold text-xl py-2 md:py-4 md:text-3xl">Total Sends within 30 Days!</div>
+            <div className="flex justify-center h-1/2 py-4 md:h-1/3 md:py-10">
+           {gradeBars}
+        </div>
         {userSends.length == 0 ? (
+            
                         <div className="flex flex-col items-center text-white">
                         <div className="font-nunito text-xl md:text-3xl my-10">Log a send!</div>
                         <button className="bg-gray-800 mb-5 font-nunito text-white text-lg rounded-md border border-white hover:bg-gray-600 hover:text-white px-4 py-2 mt-2 transition-colors duration-300" onClick={handleLogSend}>Log</button>
---
+++ frontend/src/components/GradeBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const GradeBar = ({ grade, count, total, barColor }) => {
+    const percentage = (count / total) * 100;
+
+    return (
+        <div className="mb-2">
+            <div className="flex flex-col items-center mx-2 min-h-screen">
+                <div className="bg-gray-300 h-64 w-14 flex flex-col justify-end rounded-md relative">
+                    <motion.div
+                        className="rounded-md "
+                        style={{ height: `${percentage}%`, backgroundColor: barColor }} 
+                        initial={{ height: 0 }}
+                        animate={{ height: `${percentage}%` }}
+                        transition={{ duration: 1 }}
+                    />
+                    <div className="absolute top-max left-0 w-full flex items-center justify-center font-nunito-black font-black text-gray-800">
+                        {count} 
+                    </div>
+                </div>
+                <div className="text-white font-nunito-black font-extrabold text-2xl mt-2">{grade}</div>
+            </div>
+        </div>
+    );
+};
+
+export default GradeBar;
